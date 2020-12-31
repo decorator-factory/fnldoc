ffef0326233c3975f94ab20f68c01a103de6f7ed
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2020-12-31 16:48:51 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="fnldocs",
+    name="fnldoc",
     version="0.0.1",  # don't change version for now, early development
     author="decorator-factory",
     author_email="appendix.y.z@gmail.com",
